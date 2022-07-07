ed2725f627c5f596c6016cad4a0323a31e4c201d
umesh.jangid <umesh.jangid@smartsensesolutions.com>
2022-07-07 15:48:44 +0530

    database changes 1.0

--- src/utils/dbconfig/db.config.ts
+++ src/utils/dbconfig/db.config.ts
@@ -5,21 +5,21 @@ class mysqlDBClass
     
     public sequelize
     constructor()
-    {        
+    {     
         this.sequelize = new Sequelize
         (  
-            //  process.env.DB_NAME || "" ,
-            // process.env.DB_USER || "" ,
-            // process.env.DB_PASSWORD ,
-            // {
-            //     host: process.env.DB_HOST || "", 
-            //     dialect:'mysql',
-            //     port:Number(process.env.DB_PORT) || 3306
+            process.env.DB_NAME || "" ,
+            process.env.DB_USER || "" ,
+            process.env.DB_PASSWORD ,
+            {
+                host: process.env.DB_HOST || "", 
+                dialect:'mysql',
+                port:Number(process.env.DB_PORT) || 3306
                 
                 
-            // }
-            process.env.DB_URI || ""
-        );
+            }
+            
+        );        
         this.sequelize.sync({force:true});
     }
     
